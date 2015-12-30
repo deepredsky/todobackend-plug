02f2ed2e32862c81575f66a5f3f689535c89a3e7
Rajesh Sharma <rajesh@unity3d.com>
2015-12-30 11:42:04 +0100

    update port

--- lib/todobackend/starter.ex
+++ lib/todobackend/starter.ex
@@ -1,5 +1,6 @@
 defmodule Todobackend.Starter do
   def start_link() do
-    {:ok, _} = Plug.Adapters.Cowboy.http Todobackend.Router, [], port: System.get_env("PORT") || 4000
+    port = String.to_integer( System.get_env("PORT") || "4000" )
+    {:ok, _} = Plug.Adapters.Cowboy.http Todobackend.Router, [], port: port
   end
 end
