825c0a8fd434e74601b0a41fade3346237002747
Rajesh Sharma <rajesh@unity3d.com>
2015-12-30 11:33:10 +0100

    update port for Heroku

--- lib/todobackend/starter.ex
+++ lib/todobackend/starter.ex
@@ -1,5 +1,5 @@
 defmodule Todobackend.Starter do
   def start_link() do
-    {:ok, _} = Plug.Adapters.Cowboy.http Todobackend.Router, []
+    {:ok, _} = Plug.Adapters.Cowboy.http Todobackend.Router, [], port: System.get_env("PORT") || 4000
   end
 end
